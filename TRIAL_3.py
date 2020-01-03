08278006d10d8ca47e8e981c9cf8807f7367befd
rudi4574 <53629671+rudi4574@users.noreply.github.com>
2020-01-03 04:07:29 -0800

    Add files via upload

---
+++ TRIAL_3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 13:42:01 2020
+
+@author: Rudresh
+"""
+
+'mean':[a],'Mediam':[b],'Mode':[c],'Kurtosis':[d],'skewness':[e],'Standard deviation':[f],'Variance':[g],'Standard error':[h],'Maximum':[i],'Minimum':[j],'sum':[k],'Quartile25':[m],'Quartile50':[n],'Quartile75':[o]]
